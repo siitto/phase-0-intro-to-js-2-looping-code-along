712a7216a3be19e2dce92830668d667f400db803
+ <+>
2023-11-10 00:27:14 -0500

    countdown looping and returned an array of messages

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+
+function writeCards(names, string){
+
+let newArray = [];    
+for (let i = 0; i < names.length; i++){
+    newArray.push(`Thank you, ${names[i]}, for the wonderful ${string} gift!`);
+
+}
+return newArray;
+}
+
+
+ 
+let int = 10;
+function countDown(int){
+for (int = 10; int >= 0; --int){
+  console.log(int);
+}
+    return int;
+}
+
+console.log(countDown());
